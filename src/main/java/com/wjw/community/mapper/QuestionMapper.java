12af8c0e6a3bcdd3622e8610550b7cb3ac6103eb
wjw363 <1635739073@qq.com>
2020-02-07 17:26:08 +0800

    add publish

---
+++ src/main/java/com/wjw/community/mapper/QuestionMapper.java
@@ -0,0 +1,14 @@
+package com.wjw.community.mapper;
+
+import com.wjw.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag}) ")
+    void create(Question question);
+}
