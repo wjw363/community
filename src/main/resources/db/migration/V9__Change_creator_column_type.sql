2163bae2c5251ffb5e0b9b8a99ec5dd3437a7581
wjw363 <1635739073@qq.com>
2020-02-23 19:42:57 +0800

    修复数据库脚本

--- src/main/resources/db/migration/V9__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V9__Change_creator_column_type.sql
@@ -1,2 +1,2 @@
-alter table question modify creator bigint auto_increment;
-alter table comment modify commentator bigint auto_increment;
+alter table question modify creator bigint;
+alter table comment modify commentator bigint;
