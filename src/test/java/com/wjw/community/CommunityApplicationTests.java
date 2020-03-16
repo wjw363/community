da90698c5a0d92b06ea4a38556dc33146908bd41
wjw363 <1635739073@qq.com>
2020-03-16 15:11:22 +0800

    修改test文件

--- src/test/java/com/wjw/community/CommunityApplicationTests.java
+++ src/test/java/com/wjw/community/CommunityApplicationTests.java
@@ -3,7 +3,7 @@ package com.wjw.community;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CommunityApplicationTests {
 
     @Test
